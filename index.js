29566156c3099aa620c96871fbee44e39373596f
Raksha Kagadalu <kagadaluraju.r@northeastern.edu>
2022-09-29 16:30:46 -0400

    added comments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const axios = require('axios');
 const app = express();
 
-//set my local host to port 3300
+//set my local host to port 330
 app.listen(3300,()=>{
     console.log("Server is listening on port 3300...")
 });
