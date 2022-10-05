e6dcaddae075e714bdd7a35190fef32263dbea31
Raksha Kagadalu <kagadaluraju.r@northeastern.edu>
2022-10-05 01:58:17 -0400

    added get id request services

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+//declaring all the dependencies required for this api
 const client = require("./db.js");
 const express = require("express");
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-const { response, request } = require("express");
+// const { response, request } = require("express");
 const pool = require("./db.js");
+const basicAuth = require('express-basic-auth')
 
 const app = express();
 app.use(bodyParser.json());
@@ -45,9 +47,27 @@ app.get("/healthz", async(req,res)=>{
 
 
 //get a particular id request and actions
+app.get("/v1/account/:id", async(req,res)=>{
+try{
+    const id = parseInt(req.params.id)
+pool.query(`select * from accounts where id=$1`,[id],(err,results)=>{
+    if(err) throw err;
+    else if(results.rows.length==0){
+        res.send({
+            msg: "No record found, please check your id and try again"
+        })
+    }
+    res.status(200).json(results.rows);
+})
 
+}catch(err){
+    console.error(err.message);
+    res.send({
+     msg: err.message
+ })
+}
 
-
+})
 
 
 //post http request route and actions
@@ -119,10 +139,49 @@ app.post("/v1/account",async(req,res)=>{
     
     })
     
+    app.put("/v1/account/:id",async(req,res)=>{
+        try{
+            const id = parseInt(req.params.id)
+            //check if id exist
+        pool.query(`select * from accounts where id=$1`,[id],(err,results)=>{
+            if(err) throw err;
+            else if(results.rows.length==0){
+                res.send({
+                    msg: "No record found, please check your id and try again"
+                })
+            }
+        })
+        //basic auth check
+
+
+        //check if there is no id/username/created/updated fields in req body
+
+        //check if there is no empty fields
+
+        //update the database
+
+        pool.query(``,[],)
+        res.status(200).json(results.rows);
+        
+        }catch(err){
+            console.error(err.message);
+            res.send({
+             msg: err.message
+         })
+        }
+
+    })
+
+
     //set my local host to port 3300
     app.listen(3300,()=>{
         console.log("Server is listening on port 3300...")
     });
+
+
+
+
+
 // // set swagger url as constant
 // const URL = 'https://virtserver.swaggerhub.com/fall2022-csye6225/cloud-native-webapp/assignment-01/healthz';
 
