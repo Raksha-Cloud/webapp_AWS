b136efc560f90dc8ef2adc0770c8cab4768aaf25
Raksha Kagadalu <kagadaluraju.r@northeastern.edu>
2022-10-27 12:54:13 -0400

    updated db config

--- db.js
+++ db.js
@@ -1,3 +1,22 @@
+require("dotenv").config();
+
+const { Sequelize } = require('sequelize');
+const database = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+module.exports = database;
+
+
 // //pool is a method in pg lib to access postgres
 // const Pool = require("pg").Pool;
 
@@ -16,18 +35,18 @@
 // module.exports = pool;
 
 
-const { Sequelize } = require('sequelize');
-const database = new Sequelize('postgres', 'postgres', 'admin', {
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: false,
+// const { Sequelize } = require('sequelize');
+// const database = new Sequelize('postgres', 'postgres', 'admin', {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     logging: false,
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// });
 
-module.exports = database;
+// module.exports = database;
