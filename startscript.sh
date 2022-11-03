2e4dc8a327dad9547f5f605d6144f1cc00bca125
Raksha Kagadalu <98128238+Raksha-Kagadalu@users.noreply.github.com>
2022-11-03 15:36:18 -0400

    try 2

--- startscript.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "#######using iptables to forward request coming in on port 80 to port 3000 internally.######"
-sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3300
