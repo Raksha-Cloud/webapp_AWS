24fa0892d9109f3effd02f1da84455eea5167c02
Raksha Kagadalu <98128238+Raksha-Kagadalu@users.noreply.github.com>
2022-11-07 20:16:06 -0500

    updated file path

--- shellCommands.sh
+++ shellCommands.sh
@@ -6,6 +6,7 @@ sudo chown ubuntu ./*
 echo "###################################Installing the cloudwatch agent config file to /opt/cloudwatch-config.json##################"
 sudo curl -o /root/amazon-cloudwatch-agent.deb https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E /root/amazon-cloudwatch-agent.deb
+
 echo "###################################Installing node##################"
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install nodejs -y
@@ -18,8 +19,9 @@ unzip webapp.zip
 sudo rm -rf webapp.zip
 # Copying cloudwatch agent config file
  echo "###################################Copying the cloudwatch agent config file to /opt/cloudwatch-agent-config.json##################"
- sudo cp /home/ubuntu/webapp/cloudwatch-agent-config.json /opt/cloudwatch-agent-config.json
-
+ sudo cp /home/ubuntu/cloudwatch-agent-config.json /opt/cloudwatch-agent-config.json
+ls -lrta
+pwd
  # Configuring CloudWatch Agent
  echo "######################################Configuring CloudWatch Agent#######################################################"
  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-agent-config.json -s
