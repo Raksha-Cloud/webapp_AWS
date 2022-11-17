1bb1f902af9ac960a2e4f836764ac2af51e8f52e
Raksha Kagadalu <98128238+Raksha-Kagadalu@users.noreply.github.com>
2022-11-17 16:51:06 -0500

    final test

--- Utils/snsUtils.js
+++ Utils/snsUtils.js
@@ -1,31 +1,32 @@
 const AWS = require('aws-sdk');
-const logger = require('../config/logger-config.js');
-//setup the .env file
+const appLogger = require('../config/logger-config.js');
+
+//setting up the .env file
 require('dotenv').config();
 const snsProvider = {
   publishMessage: publishMessage,
 };
 async function publishMessage(message) {
-  // configuring aws
+  // setting up the sns configuration
   const sns = new AWS.SNS({
     region: process.env.AWS_BUCKET_REGION,
   });
-  // configuring the params
+  // setting up the sns params
   const params = {
     TopicArn: process.env.SNS_TOPIC_ARN,
     Message: message,
   };
-  // publishing the message
+  // sending the sns message as trigger
   try {
-    // publish the sns message
+    // publishing the email content to user
     const messageData = await sns.publish(params).promise();
-    logger.info(
+    appLogger.info(
       `Message ${params.Message} sent to topic ${params.Topic} and messageId ${messageData.MessageId}`
     );
   } catch (err) {
-    logger.error(err);
+    appLogger.error(err);
     console.log(err);
   }
 }
-// export the snsProvider
+
 module.exports = snsProvider;
