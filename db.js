dd44eed58eac79eaca3855b59b4fa181a5811f57
Raksha Kagadalu <98128238+Raksha-Kagadalu@users.noreply.github.com>
2022-11-29 20:03:45 -0500

    commiting assignment 9 changes

--- db.js
+++ db.js
@@ -1,21 +1,28 @@
 require("dotenv").config();
+const fs = require("fs");
 
 const { Sequelize } = require('sequelize');
+
+const sslFile = fs.readFileSync(__dirname + '/global-bundle.pem');
+//const sslFile = fs.readFileSync("/home/ubuntu/global-bundle.pem");
 const database = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          rejectUnauthorized: true,
+          ca: sslFile,
+        },
     logging: false,
-
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
     }
-});
+}});
 
 module.exports = database;
-
 
 // //pool is a method in pg lib to access postgres
 // const Pool = require("pg").Pool;
